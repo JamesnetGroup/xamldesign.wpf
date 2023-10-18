c225e60e09f05bb020505ab5043bccf4da8cac42
JAEWUNG LEE <james@jamesnet.dev>
2023-10-18 12:14:09 +0900

    Updates

--- src/XamlDesign.Wpf/UI/Units/IconCheckBox.cs
+++ src/XamlDesign.Wpf/UI/Units/IconCheckBox.cs
@@ -1,10 +1,43 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace XamlDesign.Wpf.UI.Units
 {
     public class IconCheckBox : CheckBox
     {
+        #region IconBackground
+
+        public static readonly DependencyProperty IconBackgroundProperty =
+            DependencyProperty.Register(
+                "IconBackground",
+                typeof(Brush),
+                typeof(IconCheckBox),
+                new FrameworkPropertyMetadata());
+
+        public Brush IconBackground
+        {
+            get => (Brush)GetValue(IconBackgroundProperty);
+            set => SetValue(IconBackgroundProperty, value);
+        }
+        #endregion
+
+        #region IconCheckedBackground
+
+        public static readonly DependencyProperty IconCheckedBackgroundProperty =
+            DependencyProperty.Register(
+                "IconCheckedBackground",
+                typeof(Brush),
+                typeof(IconCheckBox),
+                new FrameworkPropertyMetadata());
+
+        public Brush IconCheckedBackground
+        {
+            get => (Brush)GetValue(IconCheckedBackgroundProperty);
+            set => SetValue(IconCheckedBackgroundProperty, value);
+        }
+        #endregion
+
         static IconCheckBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(IconCheckBox), new FrameworkPropertyMetadata(typeof(IconCheckBox)));
