a4fbf1c1e15a2d017f12494f58c2945ac282b80b
JAEWUNG LEE <james@jamesnet.dev>
2023-10-19 23:33:54 +0900

    Updates

--- src/XamlDesign.Wpf/UI/Units/RadioExpander.cs
+++ src/XamlDesign.Wpf/UI/Units/RadioExpander.cs
@@ -1,10 +1,26 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace XamlDesign.Wpf.UI.Units
 {
     public class RadioExpander : RadioButton
     {
+        #region HeaderBackground
+
+        public static readonly DependencyProperty HeaderBackgroundProperty =
+            DependencyProperty.Register(
+                "HeaderBackground",
+                typeof(Brush),
+                typeof(RadioExpander),
+                new FrameworkPropertyMetadata());
+
+        public Brush HeaderBackground
+        {
+            get => (Brush)GetValue(HeaderBackgroundProperty);
+            set => SetValue(HeaderBackgroundProperty, value);
+        }
+        #endregion
 
         public static readonly DependencyProperty StartDateHeaderProperty =
             DependencyProperty.Register("StartDateHeader", typeof(DateTime?), typeof(RadioExpander), new PropertyMetadata(null, CHanged));
