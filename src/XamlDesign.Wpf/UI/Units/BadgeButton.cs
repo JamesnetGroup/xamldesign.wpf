37fd7d1f2f179016bfe7bb678cbe81f514ccc257
YANG QU <quyang115@gmail.com>
2023-10-21 04:31:33 -1200

    Updates

---
+++ src/XamlDesign.Wpf/UI/Units/BadgeButton.cs
@@ -0,0 +1,17 @@
+﻿using Jamesnet.Wpf.Controls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace XamlDesign.Wpf.UI.Units
+{
+    public class BadgeButton : IconButton
+    {
+
+        static BadgeButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BadgeButton), new FrameworkPropertyMetadata(typeof(BadgeButton)));
+        }
+
+    }
+}
